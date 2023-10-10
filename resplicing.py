0c190401defb6092f42472fc393a08b41c8ccd18
Edmond <edmond@EdmonddeMacBook-Pro.local>
2023-10-10 20:17:07 +0800

    [#2]修改匹配文件
    修改匹配文件

--- resplicing.py
+++ resplicing.py
@@ -1,11 +1,17 @@
 import os
 import json
+from augconversion import texConversion
+
 
 # 存放被比较的序列
 compare_berfor_tokens = []
 ltokens = []
 rtokens = []
 orig_id = []
+#用于对比旧，左右tokens
+compare_map1 = []
+#用于对比新，type
+compare_map2 = []
 
 def compare_string_arrays(array1, array2):
     if len(array1) != len(array2):
@@ -18,37 +24,67 @@ def compare_string_arrays(array1, array2):
 
 def refix(tokens,entities):
     # 匹配
-    for i,compare in enumerate(compare_berfor_tokens):
+    flag = 0
+    not_compare = 0
+    for i,compare in enumerate(compare_map2):
         is_same_order = compare_string_arrays(tokens,compare)
 
         if(is_same_order):
             print("tokens is ",tokens ,"\n",\
-              "cpmpare with ", i)
+              "cpmpare with ", i+1)
+            print("原来的是",compare_map1[i])
+            flag = i
             break
-        if(i==len(compare_berfor_tokens)-1):
-            print("匹配不上")
-    # 拼接
-    word = {
-        'tokens'    : compare,
-        'entities'  :entities,
+        if(i==len(compare_berfor_tokens)):
+            not_compare = 1
+            print("匹配不上\n")
+
+    for i,compare in enumerate(compare_berfor_tokens):
+        if(not_compare == 1):
+            break
+        
+        is_same_order = compare_string_arrays(compare_map1[flag],compare)
+
+        if(is_same_order):
+            print("原来的token能匹配上左右tokens")
+            # print("tokens is ",tokens ,"\n",\
+            #   "cpmpare with ", i)
+            # print("原来的是",compare_map1[i])
+            flag = i
+            break
+    
+    print("\n")
+
+    if(not_compare != 1):
+                word = {
+        'tokens'    : tokens,
+        'entities'  : entities,
         'relations' : [],
-        'orig_id'   : orig_id[i],
-        'ltokens'   : ltokens[i],
-        'rtokens'   : rtokens[i]
+        'orig_id'   : orig_id[flag],
+        'ltokens'   : ltokens[flag],
+        'rtokens'   : rtokens[flag]
     }
+    else:
+        word = []
+
     return word
     
 
 
 
-def convert(aug_json_file_path,before_json_file_path,output_file_path):
+def convert(aug_json_havetype_file_path, aug_text_file, before_json_file_path, output_file_path):
     global compare_berfor_tokens
     global ltokens
     global rtokens
     global orig_id
+    global compare_map1
+    global compare_map2
     result = []
-    with open(aug_json_file_path) as f:
-        data1 = json.load(f)
+
+    # 增强文件 用于匹配
+    compare_map1 , compare_map2 = texConversion(aug_text_file)
+
+    # 原文件 用于加左右tokens
     with open(before_json_file_path) as f:
         data2 = json.load(f)
     for i in data2:
@@ -57,21 +93,34 @@ def convert(aug_json_file_path,before_json_file_path,output_file_path):
         ltokens.append(i['ltokens'])
         rtokens.append(i['rtokens'])
 
-    for i in data1:
+    # 增强文件 用于加type
+    with open(aug_json_havetype_file_path) as f:
+        data1 = json.load(f)
+    for number,i in enumerate(data1):
         tokens = i['tokens']
         entities = i['entities']
+        print("number is ",number)
         result.append(refix(tokens,entities))
-    
+
+
     with open(output_file_path, 'w', encoding='utf-8') as file:
         json.dump(result, file, ensure_ascii=False, indent=2)
 
 
 def main():
-    aug_json_file_path = './acc1_reverse.json'
-    before_json_file_path = './ace2004_dev_context.json'
-    output_file_path = './result.json'
-    convert(aug_json_file_path,before_json_file_path,output_file_path)
     
+    # aug_json_havetype_file_path = './json_file/aug_havetype_indeal.json'
+    # aug_text_file = 'aug_file/ace04_train_attn_0.3_xlm-roberta-large-0.3-false-gauss-attention-dynamic-0.3-5-false-100-xlm-large-ace04-mixup-42-retrain-test.txt'
+    # before_json_file_path = './json_file/ace2004_train_context.json'
+
+    # 假数据
+    aug_json_havetype_file_path = './fake_data/train_undeal.json'
+    aug_text_file = 'fake_data/aug.text'
+    before_json_file_path = './fake_data/train.json'
+
+    output_file_path = './output/result.json'
+
+    convert(aug_json_havetype_file_path, aug_text_file, before_json_file_path, output_file_path)
 
 if __name__ == "__main__":
     main()
