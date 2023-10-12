0108e0de1916a7b77a62a907bdee04ecf7d00884
Edmond <edmond@EdmonddeMacBook-Pro.local>
2023-10-12 08:49:56 +0800

    [#3]修复deleteO.py bug
    写入文件有问题，每次写入一行被初始化，导致最终写入只有最后一句。

--- deleteO.py
+++ deleteO.py
@@ -2,14 +2,18 @@ import os
 from conlltojson import splitTokens
 
 sentence = []
+#输出文件夹
 outputFile = './output/deleteO.conll'
-conll_file_path = './fake_data/train_undeal.conll'
+#输入文件夹ß
+conll_file_path = './step_1/ace04-equal.txt'
 
 def writeToConll(output_text):
     with open(outputFile, 'w') as file:
     # 将字符串写入文件
+        output_string = ''
         for i in output_text:
-            output_string = ''.join(i)
+            output_string += ''.join(i)
+            output_string += ''.join('\n')
         file.write(output_string)
 
 def slove(lines):
